d02acc7ba556e4788ebd78a017e5269f81708ac4
pavel <pavel_boodo@yahoo.com>
2021-11-29 19:01:34 +0100

    changes

--- models/DataTrafficWithDatabase.php
+++ models/DataTrafficWithDatabase.php
@@ -3,5 +3,27 @@
 
 class DataTrafficWithDatabase
 {
+    private $server_name = "localhost";
+    private $database_username = "root";
+    private $database_password = "";
+    private $database_name = "food";
+    private $connection = null;
 
+    public function getConnection(){
+        return $this->connect();
+    }
+
+    private function connect(){
+        if($this->connection == null){
+            $this->connection = new mysqli($this->server_name,
+                $this->database_username,
+                $this->database_password,
+                $this->database_name);
+            return $this->connection;
+        }
+        else{
+            return $this->connection;
+        }
+    }
 }
+?>
